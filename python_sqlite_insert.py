eff9d8e1cd6f17c6cfd2e46bab0034cdfe7dd6e9
Shreya240805 <shreya24.santubal@gmail.com>
2025-07-25 09:33:58 +0530

    Delete python_sqlite_insert.py

--- python_sqlite_insert.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn=sqlite3.connect('mydata.db')
-cursor=conn.cursor()
-cursor.execute("INSERT INTO users (name, age) VALUES (?,?)",("Alice",25))
-cursor.execute("INSERT INTO users (name, age) VALUES (?,?)",("Bob",30))
-conn.commit()
-conn.close()
