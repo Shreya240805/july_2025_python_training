57f447d5c3b474be5d80e8ed918a80decec4e71d
Shreya240805 <shreya24.santubal@gmail.com>
2025-07-25 09:33:24 +0530

    Delete name_age_insert.py

--- name_age_insert.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn=sqlite3.connect('mydata.db')
-cursor=conn.cursor()
-name=input("enter name:")
-age=int(input("enter age:"))
-cursor.execute("INSERT INTO users (name, age) VALUES (?,?)",(name, age))
-conn.commit()
-conn.close()
